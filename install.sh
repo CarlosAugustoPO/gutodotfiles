46e41dacf118d7990f9d7c3caa95a6e08998a613
Carlos Augusto Padilha de Oliveira <81203130+CarlosAugustoPO@users.noreply.github.com>
2021-07-02 13:03:02 -0300

    Add prototype of install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ln -s $HOME/Projects/GutoDotFiles/.bash $HOME/.bash;
+ln -s $HOME/Projects/GutoDotFiles/.bashrc $HOME/.bashrc;
+ln -s $HOME/Projects/GutoDotFiles/.profile $HOME/.profile;
+ln -s $HOME/Projects/GutoDotFiles/.tmux.conf $HOME/.tmux.conf;
+ln -s $HOME/Projects/GutoDotFiles/.bin $HOME/.bin;
+ln -s $HOME/Projects/GutoDotFiles/.tmux $HOME/.tmux;
+ln -s $HOME/Projects/GutoDotFiles/.vimrc $HOME/.vimrc;
+ln -s $HOME/Projects/GutoDotFiles/.vim $HOME/.vim;
+ln -s $HOME/Projects/GutoDotFiles/Documents/ $HOME/Documents/
+
+#TODO (Mv default files to bkpup folder)
